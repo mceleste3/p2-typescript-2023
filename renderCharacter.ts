b983b8d3a2457daf340314c57eacf6d6159a63e1
María Celeste Ortiz <maria.celeste.ortz@gmail.com>
2023-04-21 19:43:46 +0200

    Render character: Estilos añadidos
    Render characters:
    Modificación del header del html

--- renderCharacter.ts
+++ renderCharacter.ts
@@ -1,18 +1,103 @@
 import { Character } from "./characters.ts";
+import { header_body } from "./renderCharacters.ts";
 
 const head = (title: string) => `
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>${title}</title>
-<head>`;
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${title}</title>
+    <style>${header_body}
+
+            h2{
+            margin-top: 2rem;
+            font-size: 26px;
+            margin-left: 0.8rem;
+            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',Verdana, sans-serif;
+            }
+            
+            .key{
+            margin-bottom: 0.3rem;
+            color: rgb(59, 59, 59);
+            font-weight: bold;
+            font-size: 17px;
+            }
+
+            .value{
+            font-size: 14px;
+            color: rgb(52, 145, 68);
+            }
+
+            .container{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            border: 1px solid rgb(194, 175, 35);
+            background-color: white;
+            padding: 0.5rem 1rem;
+            border-radius: 15px; 
+            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',Verdana, sans-serif;
+            }
+
+            .information{
+            margin: 4rem 0;
+            display: flex;
+            justify-content: space-evenly
+            }
+
+            p{
+            margin-top: 6rem;
+            margin-left: 0.8rem;
+            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',Verdana, sans-serif;
+            }
+
+            iframe{
+            margin-left: 0.8rem;
+            height: 60%;
+            width: 50%;
+            }
+    </style>
+  <head>`;
 
 export const renderCharacter = (character: Character) => {
-    return `<html>
+    return `
+<html>
     ${head(character.name)}
     <body>
-        <h1>LOTR Charter: ${character.name}</h1>
+      <header>
+          <h1>The Lord of the Rings: Characters</h1>
+          <img src="ring.png" height="45px" width="60px">
+        </header>
+        
+        <h2>${character.name}</h2>
+        <div class="information">
+            <div class="container">
+                <span class="key">Race</span>
+                <span class="value">${character.race}</span>
+            </div>   
+                
+            <div class="container">
+                <span class="key">Gender</span>
+                <span class="value">${character.gender === ""? "-" : character.gender}</span>
+            </div>  
+                            
+            <div class="container">
+                <span class="key">Birth</span>
+                <span class="value">${character.birth === ""? "-" : character.birth}</span>
+            </div> 
+                            
+            <div class="container">
+                <span class="key">Death</span>
+                <span class="value">${character.death === ""? "-" : character.death}</span>
+            </div> 
+                      
+            <div class="container">
+                <span class="key">Spouse</span>
+                <span class="value">${character.spouse === ""? "-" : character.spouse}</span>
+            </div> 
+        </div>  
+        <p>You can see more information of the character in this web:</p>
+        <iframe src="${character.wikiUrl}"></iframe>
     </body>
 </html>`;
 };
--- renderCharacters.ts
+++ renderCharacters.ts
@@ -2,35 +2,48 @@ import { Character } from "./characters.ts";
 import { renderCharacter } from "./renderCharacter.ts";
 import { writeFile } from "fs/promises";
 
-const head = (title: string) => `
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>${title}</title>
-        <style>
+export const header_body: string = `
             body{
                 margin: 0;
-                background-color: #f1fff0;
+                background-color: #f7f4ee;
+            }
+
+            header{
+                display: flex;
+                justify-content: center;
+                background-color: rgb(226, 255, 182);
+                border-bottom: 1px solid rgb(178, 173, 24);
+                margin-bottom: 2.5rem;
             }
 
             h1{
                 text-align: center;
                 object-fit: cover;
                 font-style: italic;
-                margin-top: 0.7rem;
             }
 
+            img{
+                margin-left: 0.7rem;
+                margin-top: 1rem;
+            }`
+
+const head = (title: string) => `
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>${title}</title>
+        <style>${header_body}
             p{
                 margin-left: 0.5rem;
                 font-family: sans-serif;
             }
 
             .character{
-                text-decoration: none;
-                color: black;
                 border-radius: 7px;
                 margin: 0.5rem;
+                text-decoration: none;
+                color: black;
                 background-color: white;
                 margin-bottom: 1rem;
                 border: 1px solid rgb(84, 84, 70);
@@ -39,9 +52,10 @@ const head = (title: string) => `
                 padding: 0.5rem;
                 font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',Verdana, sans-serif;
             }
-
+            
             .character:hover{
                 border: 1px solid rgb(194, 175, 35);
+                background-color: rgb(251, 255, 245);
             }
         </style>
     <head>`;
@@ -56,7 +70,7 @@ const renderCharacters = (characters: Array<Character>) => {
     let bodyContent= "";
     let num: number = 1;
     characters.forEach(character => {
-        bodyContent += `<a class="character" target="_black" href="${generateFile(character, num)}">
+        bodyContent += `<a class="character" target="_blank" href="${generateFile(character, num)}">
             ${num}. ${character.name}
         </a>`;
         num ++;
@@ -69,8 +83,10 @@ export const render = (characters: Array<Character>) => {
 <html>
     ${head("Characters")}
     <body>
-        <h1>The Lord of the Rings: Characters</h1>
-        <hr />
+        <header>
+            <h1>The Lord of the Rings: Characters</h1>
+            <img src="ring.png" height="45px" width="60px">
+        </header>
         <p>Select the name of character for more information</p>
             ${renderCharacters(characters)}
     </body>
